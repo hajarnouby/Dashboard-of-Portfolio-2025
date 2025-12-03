d5a24c09ae1af7233618ec929c76baddce7a0950
hajarnouby <hajarnoubyart@gmail.com>
2025-12-03 21:32:12 +0200

    feat: finished login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -10,7 +10,20 @@ const Login = () => {
             <div className='div1'>
             <Txt title="Welcome Back!" />
             <Form title="Username" placeholder="Enter Username" pass='Password' place='Enter Password' />
-            
+            <div className='row sb width100'>
+                <div className='row cen'>
+                    <input type="checkbox"/><p className='p1 sp1 st2 sp2'>Remember Me?</p>
+                </div>
+                <p className='st2 g1'>Forgot your Password?</p>
+            </div>
+            <button className='logbtn'>Login</button>
+            <hr />
+            <div className='cen row cen2'>
+                <p className='st2'>Don’t have an Account?</p>
+                <p className='st2 red sp2'> Sign up here.</p>
+                
+
+            </div>
             </div>
         </div>
 
